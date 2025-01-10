c4cf5035d947358c5bf316db3c24c37d73b4bf15
norang <norang1221@gmail.com>
2025-01-10 19:16:24 +0900

    GroupChat 셀 xib 생성 및 레이아웃 구성

---
+++ TravelTalk/GroupChatItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupChatItemTableViewCell.swift
+//  TravelTalk
+//
+//  Created by Kyuhee hong on 1/10/25.
+//
+
+import UIKit
+
+class GroupChatItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

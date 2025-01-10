2b88fc64915f75db0e8c990650ee67df2a0694a6
norang <norang1221@gmail.com>
2025-01-10 17:31:28 +0900

    Storyboard, VC 파일 추가

---
+++ TravelTalk/ChatListItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatListItemTableViewCell.swift
+//  TravelTalk
+//
+//  Created by Kyuhee hong on 1/10/25.
+//
+
+import UIKit
+
+class ChatListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
